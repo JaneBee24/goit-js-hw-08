d3807a8f6431199bdb8b090c2350070350051838
JaneBee24 <bublikkjenya@gmail.com>
2023-10-20 12:52:22 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const options = {
+  closeText: 'Close',
+  captions: true,
+  captionDelay: 0,
+  loop: true
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = new SimpleLightbox('.gallery a', options);
+
+  const galleryList = document.querySelector('.gallery');
+  galleryItems.forEach(item => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = item.original;
+    link.title = item.description;
+    const image = document.createElement('img');
+    image.src = item.preview;
+    image.alt = item.description;
+    link.appendChild(image);
+    listItem.appendChild(link);
+    galleryList.appendChild(listItem);
+  });
+});
