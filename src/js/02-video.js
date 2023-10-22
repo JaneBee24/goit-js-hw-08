5cbffc92fca98cb45cf31a0ba90b27d8e2ab742c
JaneBee24 <bublikkjenya@gmail.com>
2023-10-22 22:42:27 +0200

    another time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import VimeoPlayer from 'vimeo-player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime !== null) {
+  const currentTime = parseFloat(storedTime);
+  player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle(function(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000));
+
+window.addEventListener('beforeunload', function() {
+  player.getCurrentTime().then(function(currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
 
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+
+feedbackForm.addEventListener('input', throttle(function() {
+  saveFormState();
+}, 500));
+
+window.addEventListener('load', function() {
+  loadFormState();
+});
+
+feedbackForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  clearFormState();
+  console.log({
+    email: emailInput.value,
+    message: messageTextarea.value,
+  });
+});
+
+function saveFormState() {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function loadFormState() {
+  const storedData = localStorage.getItem('feedback-form-state');
+  if (storedData !== null) {
+    const formData = JSON.parse(storedData);
+    emailInput.value = formData.email || '';
+    messageTextarea.value = formData.message || '';
+  }
+}
+
+function clearFormState() {
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageTextarea.value = '';
+}
