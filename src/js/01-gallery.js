6dec3da6b71b98dba6bcf76b9ec88c708f91e305
JaneBee24 <bublikkjenya@gmail.com>
2023-10-24 11:23:05 +0200

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,24 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 import { galleryItems } from './gallery-items';
-
+const galleryList = document.querySelector('.gallery');
 const options = {
-  closeText: 'Close',
-  captions: true,
-  captionDelay: 0,
-  loop: true
+  
+captionData: "alt",
+  captionDelay: 250,
+ 
 };
  
-document.addEventListener('DOMContentLoaded', () => {
-  const gallery = new SimpleLightbox('.gallery a', options);
-
-  const galleryList = document.querySelector('.gallery');
-  galleryItems.forEach(item => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    link.href = item.original;
-    link.title = item.description;
-    const image = document.createElement('img');
-    image.src = item.preview;
-    image.alt = item.description;
-    link.appendChild(image);
-    listItem.appendChild(link);
-    galleryList.appendChild(listItem);
-  });
-});
+const createMarkup = () =>
+  galleryItems
+    .map(({ preview, original, description } = {}) => {
+      return `
+      <div class="gallery">
+        <a href="${original}"><img src="${preview}" alt="" title="${description}"/></a>
+      </div>
+    `;
+    })
+    .join('');
+// заливаємо розмітку
+galleryList.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+const gallery = new SimpleLightbox('.gallery a', options);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-import VimeoPlayer from 'vimeo-player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new VimeoPlayer(iframe);
+const player = new Player(iframe);
 
 const storedTime = localStorage.getItem('videoplayer-current-time');
 if (storedTime !== null) {
